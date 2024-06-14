2a8b31f83a632ba86979e30c349947bdaf1ea54a
Kazuki Fujimaru <kazukifujimaru@gmail.com>
2024-06-14 23:12:16 +0700

    Mengubah view sign-in modified:   resources/views/auth/signin.blade.php

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -14,12 +14,7 @@
                         <div class="col-xl-4 col-md-6 d-flex flex-column mx-auto">
                             <div class="card card-plain mt-8">
                                 <div class="card-header pb-0 text-left bg-transparent text-center">
-                                    <h3 class="font-weight-black text-dark display-6">Welcome back</h3>
-                                    <p class="mb-0">Welcome back!</p>
-                                    <p class="mb-0">Create a new acount<br></p>
-                                    <p class="mb-0">OR Sign in with these credentials:</p>
-                                    <p class="mb-0">Email: <b>admin@corporateui.com</b></p>
-                                    <p class="mb-0">Password: <b>secret</b></p>
+                                    <h3 class="font-weight-black text-dark display-6">Login</h3>
                                 </div>
                                 <div class="text-center">
                                     @if (session('status'))
@@ -36,18 +31,18 @@
                                 <div class="card-body">
                                     <form role="form" class="text-start" method="POST" action="sign-in">
                                         @csrf
-                                        <label>Email Address</label>
+                                        <label>Alamat Email</label>
                                         <div class="mb-3">
                                             <input type="email" id="email" name="email" class="form-control"
-                                                placeholder="Enter your email address"
-                                                value="{{ old('email') ? old('email') : 'admin@corporateui.com' }}"
+                                                placeholder="Email"
+                                                value="{{ old('email') }}"
                                                 aria-label="Email" aria-describedby="email-addon">
                                         </div>
-                                        <label>Password</label>
+                                        <label>Kata Sandi</label>
                                         <div class="mb-3">
                                             <input type="password" id="password" name="password"
-                                                value="{{ old('password') ? old('password') : 'secret' }}"
-                                                class="form-control" placeholder="Enter password" aria-label="Password"
+                                                value="{{ old('password') }}"
+                                                class="form-control" placeholder="Password" aria-label="Password"
                                                 aria-describedby="password-addon">
                                         </div>
                                         <div class="d-flex align-items-center">
@@ -55,29 +50,30 @@
                                                 <input class="form-check-input" type="checkbox" value=""
                                                     id="flexCheckDefault">
                                                 <label class="font-weight-normal text-dark mb-0" for="flexCheckDefault">
-                                                    Remember for 14 days
+                                                    Ingat saya
                                                 </label>
                                             </div>
                                             <a href="{{ route('password.request') }}"
-                                                class="text-xs font-weight-bold ms-auto">Forgot
-                                                password</a>
+                                                class="text-xs font-weight-bold ms-auto">Lupa sandi</a>
                                         </div>
                                         <div class="text-center">
-                                            <button type="submit" class="btn btn-dark w-100 mt-4 mb-3">Sign in</button>
+                                            <button type="submit" class="btn btn-dark w-100 mt-4 mb-3">Masuk</button>
+                                            <div class="text-center">
+                                                <p class="font-weight-thin text-grey" style="font-size: 14px;">Atau</p>
+                                            </div>
                                             <button type="button" class="btn btn-white btn-icon w-100 mb-3">
                                                 <span class="btn-inner--icon me-1">
-                                                    <img class="w-5" src="../assets/img/logos/google-logo.svg"
-                                                        alt="google-logo" />
+                                                    <img class="w-5" src="../assets/img/logos/google-logo.svg" alt="google-logo" />
                                                 </span>
-                                                <span class="btn-inner--text">Sign in with Google</span>
+                                                <span class="btn-inner--text">Masuk dengan google</span>
                                             </button>
                                         </div>
                                     </form>
                                 </div>
                                 <div class="card-footer text-center pt-0 px-lg-2 px-1">
                                     <p class="mb-4 text-xs mx-auto">
-                                        Don't have an account?
-                                        <a href="{{ route('sign-up') }}" class="text-dark font-weight-bold">Sign up</a>
+                                        Tidak punya akun ?
+                                        <a href="{{ route('sign-up') }}" class="text-dark font-weight-bold">Daftar</a>
                                     </p>
                                 </div>
                             </div>
@@ -88,10 +84,8 @@
                                     style="background-image:url('../assets/img/image-sign-in.jpg')">
                                     <div
                                         class="blur mt-12 p-4 text-center border border-white border-radius-md position-absolute fixed-bottom m-4">
-                                        <h2 class="mt-3 text-dark font-weight-bold">Enter our global community of
-                                            developers.</h2>
-                                        <h6 class="text-dark text-sm mt-5">Copyright © 2022 Corporate UI Design System
-                                            by Creative Tim.</h6>
+                                        <h2 class="mt-3 text-dark font-weight-bold">Manage Your Organization Easily with Us</h2>
+                                        <h6 class="text-dark text-sm mt-5">Copyright © 2024 Ruang Organisasi by Ciboston Project.</h6>
                                     </div>
                                 </div>
                             </div>
