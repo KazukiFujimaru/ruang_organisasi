6f0efb6cf9dc177dcdc4d2ba1915b44d7c9f98c0
Kazuki Fujimaru <kazukifujimaru@gmail.com>
2024-06-26 21:36:36 +0700

    Penyesuaian create.blade layout

--- resources/views/organisasi/create.blade.php
+++ resources/views/organisasi/create.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-guest-layout>
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
         <div class="container-fluid py-4 px-5">
             <div class="row">
@@ -73,4 +73,4 @@
             <x-app.footer />
         </div>
     </main>
-</x-app-layout>
+</x-guest-layout>
