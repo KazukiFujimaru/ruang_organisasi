6987374b6cd75538bcc8ca5b1633dda9a6a85ce5
Kazuki Fujimaru <kazukifujimaru@gmail.com>
2024-06-27 16:00:23 +0700

    "Removed sidenav top component, updated app layout, and modified organisasi edit view"

--- resources/views/organisasi/edit.blade.php
+++ resources/views/organisasi/edit.blade.php
@@ -1,6 +1,5 @@
 <x-app-layout>
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
-        <x-app.navbar />
         <div class="container-fluid py-4 px-5">
             <div class="row">
             @if($errors->any())
